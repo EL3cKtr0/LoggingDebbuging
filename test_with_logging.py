97bc35ea381cce8fdfc852891a8851840e112dac
Luigi <luigiiazzetta98@gmail.com>
2020-11-20 16:13:33 +0100

    Added real test for triangle

--- test_with_logging.py
+++ test_with_logging.py
@@ -21,33 +21,26 @@ logger.addHandler(file)
 
 
 def istriangle(l1, l2, l3):
-    if (l1 + l2) < l3:
-        logger.error("ERROR, third number is bigger than the sum other two")
-        return True
-    if (l1 + l3) < l2:
-        logger.error("ERROR, second number is bigger than the sum other two")
-        return True
-    if (l2 + l3) < l1:
-        logger.error("ERROR, first number is bigger than the sum other two")
-        return True
 
-    return False
+    if isinstance(l1, str) or isinstance(l2, str) or isinstance(l3, str):
+        return False
+
+    if (l1 + l2) <= l3:
+        logger.error("ERROR, third number is bigger than the sum other two")
+        return False
+    if (l1 + l3) <= l2:
+        logger.error("ERROR, second number is bigger than the sum other two")
+        return False
+    if (l2 + l3) <= l1:
+        logger.error("ERROR, first number is bigger than the sum other two")
+        return False
+
+    return True
 
 
 def test_method():
-    first = input()
-    if not first.isdigit():
-        logger.error('ERROR, first number must be a digit!')
-    assert first.isdigit() == True
-    second = input()
-    if not second.isdigit():
-        logger.error('ERROR, second number must be a digit!')
-    assert second.isdigit() == True
-    third = input()
-    if not third.isdigit():
-        logger.error('ERROR, third number must be a digit!')
-    assert third.isdigit() == True
-
-    logger.debug("All 3 numbers are valid for testing")
-    assert istriangle(int(first), int(second), int(third)) == False
-    logger.debug("The 3 Numbers can be 3 sides of a triangle")
+    assert istriangle(3, 4, 7) == False  # Should return False, this is NOT a Triangle
+    assert istriangle(5, 5, 5) == True  # Should return True, this is a Triangle
+    assert istriangle(0, 0, 0) == False  # Should return False, can't be a Triangle
+    assert istriangle('ciao', 4, 7) == False  # Should return False, passing a string
+    assert istriangle(-2, 1, 1) == False  # Should return False, one side is negative
